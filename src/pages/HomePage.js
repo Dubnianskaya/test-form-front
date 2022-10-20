5f8a403e8168094b166ca9579ca6e568371c5416
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2022-10-20 13:15:46 +0300

    notification

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,28 @@
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { Container } from '../components/Container';
 import { ContactForm } from '../components/ContactForm';
 import { StyledContainer } from '../components/StyledContainer';
 import { Footer } from '../components/Footer';
+import { fetchClients } from '../services/clientsApi';
 
 export function HomePage() {
+  useEffect(() => {
+    const getClients = async () => {
+      try {
+        const data = await fetchClients();
+        if (data.length) {
+          return toast.success(
+            `We have already received ${data.length} messages. Be one of them!`
+          );
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getClients();
+  }, []);
+
   return (
     <main>
       <StyledContainer>
--- src/services/clientsApi.js
+++ src/services/clientsApi.js
@@ -4,6 +4,13 @@ import { baseURL } from '../libs/constants';
 
 axios.defaults.baseURL = baseURL;
 
+export async function fetchClients() {
+  const data = await axios
+    .get(`/clients`)
+    .then(res => res.data);
+  return data;
+}
+
 export async function addClient(client) {
   const data = await axios
     .post(`/clients/add`, client)
